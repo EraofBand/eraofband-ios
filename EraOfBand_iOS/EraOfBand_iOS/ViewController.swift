b16033438790a39ed722267e31ba92d64fe3fd6c
k2645 <k2645@naver.com>
2022-07-01 22:04:12 +0900

    프로젝트 생성

--- EraOfBand_iOS/EraOfBand_iOS/ViewController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController.swift
@@ -5,20 +5,13 @@
 //  Created by 김영현 on 2022/07/01.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
