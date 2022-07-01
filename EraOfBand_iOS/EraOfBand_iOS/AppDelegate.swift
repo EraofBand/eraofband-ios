777363e25d0ec3211229ee9d9fc937bd4462cf3b
k2645 <k2645@naver.com>
2022-07-01 21:51:23 +0900

    프로젝트 생성

---
+++ EraOfBand_iOS/EraOfBand_iOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  EraOfBand_iOS
+//
+//  Created by 김영현 on 2022/07/01.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EraOfBand_iOS
+//
+//  Created by 김영현 on 2022/07/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
