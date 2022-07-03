98acde4032c04921770feaba0abee782947f931c
xongjaemin <woals99@gmail.com>
2022-07-03 23:30:32 +0900

    style: 온보딩 뷰 개발

--- EraOfBand_iOS/EraOfBand_iOS/ViewController/OnboardingViewController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/OnboardingViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
     
+    var onboardingModel = OnboardingModel.fetchMember()
     
     /*스킵 버튼 눌렀을 때 로그인 뷰로 이동*/
     @IBAction func skipBtnTapped(_ sender: Any) {
@@ -21,7 +23,52 @@ class OnboardingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    
+        collectionView.isPagingEnabled = true
 
+    }
+    
+}
+
+extension OnboardingViewController: UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return onboardingModel.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCollectionViewCell", for: indexPath) as! OnboardingCollectionViewCell
+        
+        let onboarding = onboardingModel[indexPath.item]
+        cell.onboardingModel = onboarding
+                
+        /*page control 설정*/
+        cell.pageControl.pageIndicatorTintColor = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
+        cell.pageControl.currentPageIndicatorTintColor = UIColor(red: 0.094, green: 0.392, blue: 0.992, alpha: 1)
+        cell.pageControl.currentPage = indexPath.row
+        
+        return cell
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width:415, height: 540)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
 }
