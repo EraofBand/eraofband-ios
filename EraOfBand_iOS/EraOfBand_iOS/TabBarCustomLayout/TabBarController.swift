603d202929a09883a293795898f395711283d008
k2645 <k2645@naver.com>
2022-07-06 15:17:28 +0900

    [ect] 주석 달기

--- EraOfBand_iOS/EraOfBand_iOS/TabBarCustomLayout/TabBarController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/TabBarCustomLayout/TabBarController.swift
@@ -14,14 +14,17 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         super.init(coder: coder)
     }
     
+    // MARK: Tab Bar Icon Custom
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.delegate = self
         self.selectedIndex = 2
         
+        // 탭 바 아이콘 폰트 설정
         let font = UIFont(name: "Pretendard-Bold", size: 13)!
         
+        // 각 탭 아이콘 설정, 뷰컨트롤러 연결
         let controller1 = CommunityTabViewController()
         controller1.tabBarItem = UITabBarItem(title: "커뮤니티", image: UIImage(named: "ic_community_off"), selectedImage: UIImage(named: "ic_community_on"))
         controller1.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.font: font], for: .normal)
@@ -50,7 +53,7 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     // MARK: Home Button Custom
     func setupMiddleButton() {
         
-        // 버튼 위치,크기 커스텀
+        // 홈 탭 버튼 위치,크기 커스텀
         let middleButton = UIButton(frame: CGRect(x: (self.view.bounds.width / 2) - 33, y: 15, width: 66, height: 66))
         
         middleButton.setBackgroundImage(UIImage(named: "ic_home"), for: .normal)
