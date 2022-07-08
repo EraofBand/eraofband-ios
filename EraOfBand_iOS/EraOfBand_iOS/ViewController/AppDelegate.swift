f49c9b9b4f8875de53d1053d703a8b4b83749971
xongjaemin <woals99@gmail.com>
2022-07-08 22:18:46 +0900

    etc: 카카오 api 세팅중

--- EraOfBand_iOS/EraOfBand_iOS/ViewController/AppDelegate.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let kakaoKey = Bundle.main.kakaoKey
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        KakaoSDK.initSDK(appKey: kakaoKey)
+        
         sleep(1)
         return true
     }
