bc35e819ab998b442fbc09ecebce4dad34e43ec4
k2645 <k2645@naver.com>
2022-07-09 12:00:19 +0900

    [feat] 네비게이션바 탭 바와 연결

--- EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/MypageTabViewController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/MypageTabViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class MypageTabViewController: UIViewController {
-
+    
+    @IBOutlet weak var infoView: UIView!
+    
+    @IBOutlet weak var sessionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        infoView.layer.cornerRadius = 15
+        sessionView.layer.cornerRadius = 15
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
