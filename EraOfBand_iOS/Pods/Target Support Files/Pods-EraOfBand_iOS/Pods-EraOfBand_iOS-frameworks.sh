ea49570b6c6968667cfa109ad5bdd2f73771e642
k2645 <k2645@naver.com>
2022-07-15 00:22:59 +0900

    [ect] sdk 이슈 해결

--- EraOfBand_iOS/Pods/Target Support Files/Pods-EraOfBand_iOS/Pods-EraOfBand_iOS-frameworks.sh
+++ EraOfBand_iOS/Pods/Target Support Files/Pods-EraOfBand_iOS/Pods-EraOfBand_iOS-frameworks.sh
@@ -179,31 +179,17 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKAuth/KakaoSDKAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKCommon/KakaoSDKCommon.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKNavi/KakaoSDKNavi.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKShare/KakaoSDKShare.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKStory/KakaoSDKStory.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTalk/KakaoSDKTalk.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTemplate/KakaoSDKTemplate.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKUser/KakaoSDKUser.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pageboy/Pageboy.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Tabman/Tabman.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KakaoSDKFriend/KakaoSDKFriend.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKAuth/KakaoSDKAuth.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKCommon/KakaoSDKCommon.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKNavi/KakaoSDKNavi.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKShare/KakaoSDKShare.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKStory/KakaoSDKStory.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTalk/KakaoSDKTalk.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKTemplate/KakaoSDKTemplate.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/KakaoSDKUser/KakaoSDKUser.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Pageboy/Pageboy.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Tabman/Tabman.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KakaoSDKFriend/KakaoSDKFriend.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
