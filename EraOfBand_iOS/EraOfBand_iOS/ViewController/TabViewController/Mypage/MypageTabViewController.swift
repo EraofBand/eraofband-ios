fe170a81b5e86ca0a0192a1cbc9be67dfed60187
k2645 <k2645@naver.com>
2022-07-16 18:36:21 +0900

    [feat] 마이페이지 유저 정보 서버에서 가져오기

--- EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Mypage/MypageTabViewController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Mypage/MypageTabViewController.swift
@@ -26,8 +26,6 @@ class MypageTabViewController: UIViewController {
     @IBOutlet weak var porfolLabel: UILabel!
     @IBOutlet weak var sessionLabel: UILabel!
     
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    let userIdx: String = "12"
     
     var userRegion: String = ""
     var userAge: Int = 0
@@ -36,62 +34,67 @@ class MypageTabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let header: HTTPHeaders = ["Content-Type": "application/json", "Accept": "application/json"]
-        AF.request(appDelegate.baseUrl + "/users/" + userIdx,
-                   method: .get,
-                   parameters: nil,
-                   encoding: JSONEncoding.default,
-                   headers: header)
-        .responseDecodable(of: UserInfoData.self) { response in
-            
-            print(response.value)
-            
-            guard let data = response.data else { return }
-            guard let userInfo = try? JSONDecoder().decode(UserInfoData.self, from: data) else { return }
-            
-            /*마이페이지 유저 정보 입력*/
-            self.nickNameLabel.text = userInfo.nickName
-            
-            guard let region = userInfo.region else { return }
-            self.userRegion = region.components(separatedBy: " ")[1]
-            
-            let year = DateFormatter()
-            year.dateFormat = "yyyy"
-            let currentYear = year.string(from: Date())
-            let birthYear = userInfo.birth.components(separatedBy: "-")[0]
-            self.userAge = Int(currentYear)! - Int(birthYear)! + 1
-            
-            guard let gender = userInfo.gender else { return }
-            if gender == "MALE" {
-                self.userGender = "남"
-            } else {
-                self.userGender = "여"
+        GetUserDataService.shared.getUserInfo { (response) in
+            switch(response) {
+            case .success(let userData):
+                /*서버 연동 성공*/
+                print(userData)
+                if let data = userData as? User {
+                    let data = data.getUser
+                    /*마이페이지 유저 정보 입력*/
+                    self.nickNameLabel.text = data.nickName
+
+                    let region = data.region
+                    self.userRegion = region.components(separatedBy: " ")[1]
+
+                    let year = DateFormatter()
+                    year.dateFormat = "yyyy"
+                    let currentYear = year.string(from: Date())
+                    let birthYear = data.birth.components(separatedBy: "-")[0]
+                    self.userAge = Int(currentYear)! - Int(birthYear)! + 1
+
+                    let gender = data.gender
+                    if gender == "MALE" {
+                        self.userGender = "남"
+                    } else {
+                        self.userGender = "여"
+                    }
+                    self.userInfoLabel.text = "\(self.userRegion) / \(self.userAge) / \(self.userGender)"
+
+                    if let introduction = data.introduction {
+                        self.introductionLabel.text = introduction
+                    } else {
+                        self.introductionLabel.text = ""
+                    }
+
+                    let followeeCount = data.followeeCount
+                    self.followingButton.setTitle(String(followeeCount), for: .normal)
+                    /*
+                     self.followingButton.titleLabel!.font = UIFont(name: "Pretendard-Bold", size: 40)
+                     */
+
+                    let followerCount = data.followerCount
+                    self.followerButton.setTitle(String(followerCount), for: .normal)
+
+                    let imageUrl = data.profileImgUrl
+                    if let url = URL(string: imageUrl) {
+                        self.userImageView.load(url: url)
+                    } else {
+                        self.userImageView.image = UIImage(named: "default_image")
+                    }
+                    self.userImageView.setRounded()
+
+                }
+                
+            case .requestErr(let message) :
+                print("requestErr", message)
+            case .pathErr :
+                print("pathErr")
+            case .serverErr :
+                print("serveErr")
+            case .networkFail:
+                print("networkFail")
             }
-            self.userInfoLabel.text = "\(self.userRegion) / \(self.userAge) / \(self.userGender)"
-            
-            if let introduction = userInfo.introduction {
-                self.introductionLabel.text = introduction
-            } else {
-                self.introductionLabel.text = ""
-            }
-            
-            if let followeeCount = userInfo.followeeCount {
-                self.followingButton.setTitle(String(followeeCount), for: .normal)
-                /*
-                 self.followingButton.titleLabel!.font = UIFont(name: "Pretendard-Bold", size: 40)
-                */
-            } else { return }
-            
-            if let followerCount = userInfo.followerCount {
-                self.followerButton.setTitle(String(followerCount), for: .normal)
-            } else { return }
-            
-            if let imageUrl = userInfo.profileImgUrl {
-                let url = URL(string: imageUrl)
-                self.userImageView.load(url: url!)
-                self.userImageView.setRounded()
-            }
-            
         }
         
         // 프로필 view, 세션 view 모서리 둥글게
