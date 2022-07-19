615c7501241a770df3dc39113bcda6933e87dcea
xongjaemin <woals99@gmail.com>
2022-07-20 01:04:56 +0900

    style: 팔로우 레이아웃 진행중

---
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Mypage/FollowTab/FollowTabManViewController.swift
@@ -0,0 +1,88 @@
+//
+//  FollowTableViewController.swift
+//  EraOfBand_iOS
+//
+//  Created by 송재민 on 2022/07/19.
+//
+
+import UIKit
+import Tabman
+import Pageboy
+
+class FollowTabManViewController: TabmanViewController{
+    
+    private var viewControllers: Array<UIViewController> = []
+    var myNickName: String?
+    
+    @IBAction func backBtnTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = myNickName
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        let followingVC = UIStoryboard.init(name: "FollowingTable", bundle: nil).instantiateViewController(withIdentifier: "FollowingTableViewController") as! FollowingTableViewController
+        
+        let followerVC = UIStoryboard.init(name: "FollowingTable", bundle: nil).instantiateViewController(withIdentifier: "FollowerTableViewController") as! FollowerTableViewController
+        
+        viewControllers.append(followingVC)
+        viewControllers.append(followerVC)
+        
+        self.dataSource = self
+        
+        let bar = TMBar.ButtonBar()
+        
+        addBar(bar, dataSource: self, at: .top)
+        self.settingTabBar(ctBar: bar)
+    }
+}
+
+extension FollowTabManViewController: PageboyViewControllerDataSource, TMBarDataSource{
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        
+        switch index {
+        case 0:
+            return TMBarItem(title: "팔로잉")
+        case 1:
+            return TMBarItem(title: "팔로워")
+        default:
+            let title = "Page \(index)"
+            return TMBarItem(title: title)
+        }
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
+    
+    func settingTabBar (ctBar : TMBar.ButtonBar) {
+        ctBar.layout.transitionStyle = .snap
+        
+        ctBar.layout.contentMode = .fit
+        
+        ctBar.backgroundView.style = .clear
+        
+        // 선택 / 안선택 색 + font size
+        ctBar.buttons.customize { (button) in
+            button.tintColor = UIColor(named: "on_icon_color")
+            button.selectedTintColor = .white
+            button.font = UIFont.systemFont(ofSize: 16)
+            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+        }
+        
+        // 인디케이터
+        ctBar.indicator.weight = .custom(value: 2)
+        ctBar.indicator.tintColor = UIColor(named: "on_icon_color")
+    }
+}
+
+
