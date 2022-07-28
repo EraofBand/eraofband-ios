1ed32fa9b8ff234d57a410ffe7c744c997bd1207
k2645 <k2645@naver.com>
2022-07-29 00:33:02 +0900

    [ect] 밴드생성 세션모집 탭 만들 예정

---
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Home/SessionRecruitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SessionRecruitViewController.swift
+//  EraOfBand_iOS
+//
+//  Created by 김영현 on 2022/07/28.
+//
+
+import UIKit
+
+class SessionRecruitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
