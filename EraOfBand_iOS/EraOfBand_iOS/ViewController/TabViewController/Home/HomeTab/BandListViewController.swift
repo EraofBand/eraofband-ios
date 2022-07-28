2f0437ce737ff59f02337ece844e4963f8822a91
k2645 <k2645@naver.com>
2022-07-28 20:58:10 +0900

    [feat] 레슨 리스트 서버연동, 탭바 버튼으로 변경

--- EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Home/HomeTab/BandListViewController.swift
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Home/HomeTab/BandListViewController.swift
@@ -1,23 +1,24 @@
 //
-//  LessonMatchViewController.swift
+//  BandListViewController.swift
 //  EraOfBand_iOS
 //
-//  Created by 김영현 on 2022/07/23.
+//  Created by 김영현 on 2022/07/25.
 //
 
 import UIKit
 
-class LessonMatchViewController: UIViewController {
-
+class BandListViewController: UIViewController {
+    
     @IBOutlet weak var choiceCityButton: UIButton!
     @IBOutlet weak var sessionChoiceCollectionView: UICollectionView!
-    @IBOutlet weak var LessonListTableView: UITableView!
+    @IBOutlet weak var bandListTableView: UITableView!
     
     var session: [String] = ["전체", "보컬", "기타", "베이스", "키보드", "드럼"]
     var region: String = "전체"
-    var lessonList: [lessonInfo] = []
+    var bandList: [bandInfo] = []
     var sessionNum: Int = 5
     
+    /* 지역 선택 버튼 설정 */
     func setCityButton() {
         
         var commands: [UIAction] = []
@@ -28,7 +29,7 @@ class LessonMatchViewController: UIViewController {
                 print("name: \(name)")
                 
                 self.region = name
-                self.getLessonList()
+                self.getBandList()
                 
             })
             
@@ -56,16 +57,54 @@ class LessonMatchViewController: UIViewController {
         
     }
     
-    func getLessonList() {
+    /* 네비게이션 바 커스텀 */
+    func setNavigationBar() {
         
-        GetLessonListService.getLessonInfoList(region, String(sessionNum)) { [self] (isSuccess, response) in
+        self.navigationItem.title = "생성된 밴드 목록"
+        
+        var rightBarButtons: [UIBarButtonItem] = []
+        
+        let searchImage = UIImage(named: "ic_search")
+        let searchButton = UIButton()
+        searchButton.backgroundColor = .clear
+        searchButton.setImage(searchImage, for: .normal)
+        searchButton.addTarget(self, action: #selector(self.searchButtonClicked), for: .touchUpInside)
+        
+        let searchBarButton = UIBarButtonItem(customView: searchButton)
+        let currWidth = searchBarButton.customView?.widthAnchor.constraint(equalToConstant: 20)
+        currWidth?.isActive = true
+        let currheight = searchBarButton.customView?.heightAnchor.constraint(equalToConstant: 20)
+        currheight?.isActive = true
+        
+        let negativeSpacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace,
+                                             target: nil, action: nil)
+        negativeSpacer.width = 15
+        
+        rightBarButtons.append(negativeSpacer)
+        rightBarButtons.append(searchBarButton)
+        
+        self.navigationItem.rightBarButtonItems = rightBarButtons
+        
+    }
+    
+    /* 네비게이션 바 찾기 버튼 눌렀을 때 함수 */
+    @objc func searchButtonClicked(_ sender: UIButton) {
+        
+        print("찾기 버튼 누름")
+        
+    }
+    
+    /* 밴드 리스트 불러오는 함수 */
+    func getBandList() {
+        
+        GetBandListService.getBandInfoList(region, String(sessionNum)) { [self] (isSuccess, response) in
             if isSuccess {
-                lessonList = response.result
+                bandList = response.result
                 
-                LessonListTableView.delegate = self
-                LessonListTableView.dataSource = self
+                bandListTableView.delegate = self
+                bandListTableView.dataSource = self
                 
-                LessonListTableView.reloadData()
+                bandListTableView.reloadData()
                 
             }
         }
@@ -75,19 +114,19 @@ class LessonMatchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setNavigationBar()
+        
         setCityButton()
         
-        getLessonList()
+        getBandList()
         
         sessionChoiceCollectionView.delegate = self
         sessionChoiceCollectionView.dataSource = self
-        
     }
-    
 
 }
 
-extension LessonMatchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension BandListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return session.count
@@ -117,13 +156,13 @@ extension LessonMatchViewController: UICollectionViewDelegate, UICollectionViewD
         
         print("selected index: \(sessionNum)")
         
-        getLessonList()
+        getBandList()
         
     }
     
 }
 
-extension LessonMatchViewController: UICollectionViewDelegateFlowLayout {
+extension BandListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         return CGSize(width: 50, height: 28)
@@ -136,10 +175,10 @@ extension LessonMatchViewController: UICollectionViewDelegateFlowLayout {
     
 }
 
-extension LessonMatchViewController: UITableViewDelegate, UITableViewDataSource {
+extension BandListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lessonList.count
+        return bandList.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -147,13 +186,13 @@ extension LessonMatchViewController: UITableViewDelegate, UITableViewDataSource 
         
         cell.backgroundColor = .clear
         
-        let lessoninfo = lessonList[indexPath.item]
-        let url = URL(string: lessoninfo.lessonImgUrl)!
+        let bandinfo = bandList[indexPath.item]
+        let url = URL(string: bandinfo.bandImgUrl)!
         cell.tableImageView.load(url: url)
         cell.tableImageView.contentMode = .scaleAspectFill
-        cell.tableRegionLabel.text = lessoninfo.lessonRegion
-        cell.tableTitleLabel.text = lessoninfo.lessonTitle
-        cell.tableIntroLabel.text = lessoninfo.lessonIntroduction
+        cell.tableRegionLabel.text = bandinfo.bandRegion
+        cell.tableTitleLabel.text = bandinfo.bandTitle
+        cell.tableIntroLabel.text = bandinfo.bandIntroduction
         
         return cell
     }
