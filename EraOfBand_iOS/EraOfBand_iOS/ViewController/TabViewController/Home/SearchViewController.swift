7bfb03b0cbbffe2dc96f6a7b55ef49377811f75c
k2645 <k2645@naver.com>
2022-08-05 00:12:16 +0900

    [feat] 검색 만드는중...

---
+++ EraOfBand_iOS/EraOfBand_iOS/ViewController/TabViewController/Home/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  EraOfBand_iOS
+//
+//  Created by 김영현 on 2022/08/04.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
